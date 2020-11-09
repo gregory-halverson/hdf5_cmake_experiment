454f7df2ee4285893c8d12e72008fa498b092e2c
Gregory Halverson <gregory.halverson@gmail.com>
2020-11-09 13:38:20 -0800

    Initial commit

---
+++ CMakeFiles/hdf5_cmake_experiment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hdf5_cmake_experiment.dir/main.c.o"
+  "hdf5_cmake_experiment"
+  "hdf5_cmake_experiment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hdf5_cmake_experiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
